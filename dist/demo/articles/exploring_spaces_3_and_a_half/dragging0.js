c735ae80cbf0e640737fc073cfdbb38880046b0d
vezwork <key.draw@gmail.com>
2024-05-30 16:18:34 -0400

    Towards arbitrary precision number line

--- dist/demo/articles/exploring_spaces_3_and_a_half/dragging0.js
+++ dist/demo/articles/exploring_spaces_3_and_a_half/dragging0.js
@@ -1,72 +1,12 @@
-import { clamp, log } from "../../../lib/math/Number.js";
+import { clamp } from "../../../lib/math/Number.js";
 import { BaseExpNumerals, bigNumberToRadixNumber, changeTrailingZeroesIntoBaseExponent, displayRadixNumeralsBase10OrLess, } from "./numerals.js";
+import { makeDraggable } from "./makeDraggable.js";
+import { mod } from "../../../lib/math/Number.js";
 let BASE = 10;
 window.setBase = (b) => (BASE = b);
 let SPACING = 40;
 window.setSpacing = (n) => (SPACING = n);
-const SVG_WIDTH_SCALE = 0.8;
-function makeDraggable(state, el) {
-    const evCache = [];
-    let prevDiff = -1;
-    function removeEvent(ev) {
-        // Remove this event from the target's cache
-        const index = evCache.findIndex((cachedEv) => cachedEv.pointerId === ev.pointerId);
-        evCache.splice(index, 1);
-    }
-    // from https://www.redblobgames.com/making-of/draggable/
-    // modified to use https://developer.mozilla.org/en-US/docs/Web/API/Pointer_events/Pinch_zoom_gestures
-    function start(event) {
-        if (event.button !== 0)
-            return; // left button only
-        //if (event.target instanceof SVGTextElement) return;
-        let { x, y } = state.eventToCoordinates(event);
-        state.dragging = { dx: state.pos - x };
-        el.classList.add("dragging");
-        el.setPointerCapture(event.pointerId);
-        //multitouch
-        evCache.push(event);
-    }
-    function end(ev) {
-        state.dragging = null;
-        el.classList.remove("dragging");
-        //multitouch
-        removeEvent(ev);
-        // If the number of pointers down is less than two then reset diff tracker
-        if (evCache.length < 2) {
-            prevDiff = -1;
-        }
-    }
-    function move(ev) {
-        //multitouch
-        // Find this event in the cache and update its record with this event
-        const index = evCache.findIndex((cachedEv) => cachedEv.pointerId === ev.pointerId);
-        evCache[index] = ev;
-        // If two pointers are down, check for pinch gestures
-        if (evCache.length === 2) {
-            // Calculate the distance between the two pointers
-            const curDiff = Math.abs(evCache[0].clientX - evCache[1].clientX);
-            if (prevDiff > 0) {
-                const diff = curDiff - prevDiff;
-                zoom *= 1.01 ** diff;
-            }
-            // Cache the distance for the next move event
-            prevDiff = curDiff;
-            return;
-        }
-        //single touch
-        if (!state.dragging)
-            return;
-        ev.preventDefault();
-        let { x, y } = state.eventToCoordinates(ev);
-        state.pos = x + state.dragging.dx;
-    }
-    el.addEventListener("pointerdown", start);
-    el.addEventListener("pointerup", end);
-    el.addEventListener("pointercancel", end);
-    el.addEventListener("pointermove", move);
-    el.addEventListener("touchstart", (e) => e.preventDefault());
-}
-export const lines = document.querySelectorAll("#dragging0 .line");
+const SVG_WIDTH_SCALE = 1;
 const svg = document.querySelector("#dragging0");
 const createLine = () => {
     const g = document.createElementNS("http://www.w3.org/2000/svg", "g");
@@ -98,71 +38,42 @@ const getLine = (i) => {
     else
         return createLine();
 };
-let zoom = 1;
-svg.addEventListener("wheel", (e) => {
-    e.preventDefault();
-    if (e.ctrlKey) {
-        // for some reason this is pinch-zoom?
-        zoom *= Math.exp(-e.deltaY / 100);
-    }
-    else {
-        zoom *= Math.exp(-e.deltaY / 600);
-    }
-});
-const keysDown = {};
-svg.addEventListener("keydown", (e) => {
-    if (e.key === "ArrowLeft" ||
-        e.key === "ArrowRight" ||
-        e.key === "ArrowUp" ||
-        e.key === "ArrowDown") {
-        e.preventDefault();
-    }
-    keysDown[e.key] = true;
-});
-svg.addEventListener("keyup", (e) => {
-    keysDown[e.key] = false;
-});
+let zoom = 0;
 function draw() {
-    //console.log(keysDown);
-    if (keysDown["ArrowLeft"])
-        state._pos += 1.5 / zoom;
-    if (keysDown["ArrowRight"])
-        state._pos -= 1.5 / zoom;
-    if (keysDown["ArrowUp"])
-        zoom *= Math.exp(1 / 100);
-    if (keysDown["ArrowDown"])
-        zoom *= Math.exp(-1 / 100);
-    // seems to result in better performance than rendering in the input handlers
     requestAnimationFrame(draw);
     state.render();
 }
 requestAnimationFrame(draw);
 let state = {
-    eventToCoordinates(event) {
-        return { x: event.clientX / zoom, y: event.clientY };
-    },
     dragging: null,
     // my coordinates are messed up, so this is the pos value for pi.
     // see the first line of `render()` to see why.
-    _pos: -Math.PI * SPACING,
+    _pos: Math.PI,
     get pos() {
         return this._pos;
     },
     set pos(x) {
         this._pos = x;
-        //this.render();
     },
     render() {
         // scale and negate pos so dragging is in the right direction and not too fast.
-        const x = -this._pos / SPACING;
+        const x = this._pos;
         // NUM, ZOOM, LAYOUT CALC
+        const zoomBaseExponent = zoom / 3000;
+        const zz = BASE ** zoomBaseExponent;
         const halfWidth = (svg.clientWidth * SVG_WIDTH_SCALE) / 2;
-        const halfWidthPerNum = halfWidth / (SPACING * zoom);
-        const zoomBaseExponent = log(zoom, BASE);
+        const thing = Math.floor(halfWidth / SPACING); // should be ceil
+        const maxIterationsForHalfTicks = thing * 10 ** mod(-zoomBaseExponent, 1);
         const baseExponent = -Math.ceil(zoomBaseExponent);
         const numTickGap = BASE ** baseExponent;
-        const startInGapBase = BigInt(Math.round(x / numTickGap));
-        const startX = Math.round(x / numTickGap) * numTickGap;
+        // as you zoom in this uses more of x, as you zoom out it truncates
+        const startInGapBase = Math.round(x / numTickGap);
+        const startX = startInGapBase * numTickGap;
+        // e.g.
+        // baseExponent:   -3
+        // numTickGap:     0.001
+        // startInGapBase: 3142
+        // startX:         3.142
         // SVG INSERTION
         let linei = 0;
         svg.style.border = "none";
@@ -170,7 +81,8 @@ let state = {
         // Start in the middle and render out to the right:
         let numInGapBase = startInGapBase;
         let num = startX; // num = numInGapBase * BigInt(gap);
-        while (num < x + halfWidthPerNum) {
+        let i = 0;
+        while (i <= maxIterationsForHalfTicks) {
             renderNumberTick(num, BaseExpNumerals(numInGapBase, BASE, baseExponent));
             numInGapBase++;
             let prevNum = num;
@@ -179,11 +91,13 @@ let state = {
                 svg.style.border = "1px solid red";
                 throw "Floating Point Error!";
             }
+            i++;
         }
         // Start one unit left from the middle and render out to the left:
-        numInGapBase = startInGapBase - 1n;
+        numInGapBase = startInGapBase - 1;
         num = startX - numTickGap; // num = numInGapBase * BigInt(gap);
-        while (num > x - halfWidthPerNum) {
+        i = 1;
+        while (i <= maxIterationsForHalfTicks) {
             renderNumberTick(num, BaseExpNumerals(numInGapBase, BASE, baseExponent));
             numInGapBase--;
             let prevNum = num;
@@ -192,9 +106,10 @@ let state = {
                 svg.style.border = "1px solid red";
                 throw "Floating Point Error!";
             }
+            i++;
         }
         function numberTickX(n) {
-            return (n - x) * SPACING * zoom;
+            return (n - x) * SPACING * zz;
         }
         function renderNumberTick(n, big) {
             const radixNum = bigNumberToRadixNumber(big);
@@ -228,7 +143,13 @@ let state = {
         }
     },
 };
-makeDraggable(state, document.querySelector("#dragging0"));
+makeDraggable(document.querySelector("#dragging0"), (event) => ({ x: event.clientX, y: event.clientY }), ([x, y]) => {
+    const zoomBaseExponent = zoom / 3000;
+    const zz = BASE ** zoomBaseExponent;
+    state.pos -= x / zz / 40;
+}, (z) => {
+    zoom += z;
+});
 // make number line viewbox based on window width
 // so the number line doesn't get too small on mobile
 const el = document.querySelector("#dragging0");
---
+++ dist/demo/articles/exploring_spaces_3_and_a_half/makeDraggable.js
@@ -0,0 +1,105 @@
+import { sub } from "../../../lib/math/Vec2.js";
+// from https://www.redblobgames.com/making-of/draggable/
+// modified to use https://developer.mozilla.org/en-US/docs/Web/API/Pointer_events/Pinch_zoom_gestures
+// modified to pass diffs
+export function makeDraggable(el, eventToCoordinates, onMove = ([x, y]) => { }, onZoom = (n) => { }) {
+    const evCache = [];
+    let prevDiff = -1;
+    let pos = [0, 0];
+    let dragging = null;
+    function removeEvent(ev) {
+        // Remove this event from the target's cache
+        const index = evCache.findIndex((cachedEv) => cachedEv.pointerId === ev.pointerId);
+        evCache.splice(index, 1);
+    }
+    function start(event) {
+        if (event.button !== 0)
+            return; // left button only
+        let { x, y } = eventToCoordinates(event);
+        dragging = [pos[0] - x, pos[1] - y];
+        el.classList.add("dragging");
+        el.setPointerCapture(event.pointerId);
+        //multitouch
+        evCache.push(event);
+    }
+    function end(ev) {
+        dragging = null;
+        el.classList.remove("dragging");
+        //multitouch
+        removeEvent(ev);
+        // If the number of pointers down is less than two then reset diff tracker
+        if (evCache.length < 2) {
+            prevDiff = -1;
+        }
+    }
+    function move(ev) {
+        //multitouch
+        // Find this event in the cache and update its record with this event
+        const index = evCache.findIndex((cachedEv) => cachedEv.pointerId === ev.pointerId);
+        evCache[index] = ev;
+        // If two pointers are down, check for pinch gestures
+        if (evCache.length === 2) {
+            // Calculate the distance between the two pointers
+            const curDiff = Math.abs(evCache[0].clientX - evCache[1].clientX);
+            if (prevDiff > 0) {
+                const diff = curDiff - prevDiff;
+                onZoom(diff);
+            }
+            // Cache the distance for the next move event
+            prevDiff = curDiff;
+            return;
+        }
+        if (dragging === null)
+            return;
+        let { x, y } = eventToCoordinates(ev);
+        ev.preventDefault();
+        const newPos = [x + dragging[0], y + dragging[1]];
+        const diff = sub(newPos, pos);
+        pos = newPos;
+        onMove(diff);
+    }
+    function wheel(e) {
+        e.preventDefault();
+        if (e.ctrlKey) {
+            // for some reason this is pinch-zoom?
+            onZoom(-e.deltaY * 6);
+        }
+        else {
+            onZoom(-e.deltaY);
+        }
+    }
+    const keysDown = {};
+    function keydown(e) {
+        if (e.key === "ArrowLeft" ||
+            e.key === "ArrowRight" ||
+            e.key === "ArrowUp" ||
+            e.key === "ArrowDown") {
+            e.preventDefault();
+        }
+        keysDown[e.key] = true;
+    }
+    function keyup(e) {
+        keysDown[e.key] = false;
+    }
+    function arrowKeyAnimationFrame() {
+        // seems to result in better performance than rendering in the input handlers
+        requestAnimationFrame(arrowKeyAnimationFrame);
+        if (keysDown["ArrowLeft"])
+            onMove([1.5, 0]);
+        if (keysDown["ArrowRight"])
+            onMove([-1.5, 0]);
+        if (keysDown["ArrowUp"])
+            onZoom(1);
+        if (keysDown["ArrowDown"])
+            onZoom(-1);
+    }
+    requestAnimationFrame(arrowKeyAnimationFrame);
+    el.addEventListener("keydown", keydown);
+    el.addEventListener("keyup", keyup);
+    el.addEventListener("wheel", wheel);
+    el.addEventListener("pointerdown", start);
+    el.addEventListener("pointerup", end);
+    el.addEventListener("pointercancel", end);
+    el.addEventListener("pointermove", move);
+    el.addEventListener("touchstart", (e) => e.preventDefault());
+}

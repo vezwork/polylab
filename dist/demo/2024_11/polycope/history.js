c142411bfbd87e502cf7c0699cda47fea7c60297
vezwork <key.draw@gmail.com>
2024-12-21 00:08:06 -0700

    polycope history experiment and stuff

--- dist/demo/2024_11/polycope/history.js
+++ dist/demo/2024_11/polycope/history.js
@@ -10,11 +10,41 @@ export const history = () => {
         node.next.push(newNode);
         return newNode;
     };
+    const remove = (node) => {
+        if (!node.parent)
+            throw "attempting to remove root";
+        node.removed = true;
+        return parent;
+    };
+    const restore = (node) => {
+        node.removed = false;
+    };
+    const restoreFirstThat = (pred) => {
+        let cur = historyRoot;
+        while (cur.next.at(-1)) {
+            if (cur.removed && pred(cur.data)) {
+                restore(cur);
+                return cur.data;
+            }
+            cur = cur.next.at(-1);
+        }
+    };
+    const removeLastThat = (pred) => {
+        let cur = historyHead;
+        while (cur.parent) {
+            if (!cur.removed && pred(cur.data)) {
+                remove(cur);
+                return cur.data;
+            }
+            cur = cur.parent;
+        }
+    };
     const pushHistory = (data) => {
         historyHead = addHistoryItemAtNode(historyHead)(data);
         return historyHead.data;
     };
     const undo = () => {
+        // TODO: skip removed
         const cur = historyHead;
         historyHead = historyHead.parent ?? historyHead;
         return [historyHead.data, cur.data];
@@ -31,9 +61,17 @@ export const history = () => {
             if (cur === historyHead)
                 break;
             cur = cur.next.at(-1);
-            result.push(cur.data);
+            if (!cur.removed)
+                result.push(cur.data);
         }
         return result;
     };
-    return { pushHistory, undo, redo, mainline };
+    return {
+        pushHistory,
+        undo,
+        redo,
+        mainline,
+        removeLastThat,
+        restoreFirstThat,
+    };
 };
--- src/demo/2024_11/polycope/history.ts
+++ src/demo/2024_11/polycope/history.ts
@@ -11,15 +11,45 @@ export const history = () => {
     node.next.push(newNode);
     return newNode;
   };
+  const remove = (node) => {
+    if (!node.parent) throw "attempting to remove root";
+    node.removed = true;
+    return parent;
+  };
+  const restore = (node) => {
+    node.removed = false;
+  };
+  const restoreFirstThat = (pred) => {
+    let cur = historyRoot;
+    while (cur.next.at(-1)) {
+      if (cur.removed && pred(cur.data)) {
+        restore(cur);
+        return cur.data;
+      }
+      cur = cur.next.at(-1);
+    }
+  };
+  const removeLastThat = (pred) => {
+    let cur = historyHead;
+    while (cur.parent) {
+      if (!cur.removed && pred(cur.data)) {
+        remove(cur);
+        return cur.data;
+      }
+      cur = cur.parent;
+    }
+  };
   const pushHistory = (data) => {
     historyHead = addHistoryItemAtNode(historyHead)(data);
     return historyHead.data;
   };
   const undo = () => {
+    // TODO: skip removed
     const cur = historyHead;
     historyHead = historyHead.parent ?? historyHead;
     return [historyHead.data, cur.data];
   };
+
   const redo = () => {
     const cur = historyHead;
     historyHead = historyHead.next.at(-1) ?? historyHead;
@@ -31,10 +61,17 @@ export const history = () => {
     while (cur.next.at(-1)) {
       if (cur === historyHead) break;
       cur = cur.next.at(-1);
-      result.push(cur.data);
+      if (!cur.removed) result.push(cur.data);
     }
     return result;
   };
 
-  return { pushHistory, undo, redo, mainline };
+  return {
+    pushHistory,
+    undo,
+    redo,
+    mainline,
+    removeLastThat,
+    restoreFirstThat,
+  };
 };

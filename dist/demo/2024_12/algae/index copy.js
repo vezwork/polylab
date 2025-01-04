61ba4ad6507d14176d8d94045c3bfd2e4ebe6c9f
vezwork <key.draw@gmail.com>
2025-01-04 11:41:17 -0700

    algae

---
+++ dist/demo/2024_12/algae/index copy.js
@@ -0,0 +1,242 @@
+import { randomNiceCssOklab } from "../../../lib/math/Color.js";
+import { translation, inv as tInv, id, _, approxEq, assign, } from "../../../lib/math/CtxTransform.js";
+import { v } from "../../../lib/math/Vec2.js";
+import { create, edge, inv } from "./lib.js";
+const svg = document.getElementById("s");
+function makeCircleSvgEl(color) {
+    const circle = document.createElementNS(svg.namespaceURI, "circle");
+    circle.setAttribute("r", "4");
+    circle.setAttribute("fill", color);
+    svg.append(circle);
+    return circle;
+}
+function makeLineSvgEl(color) {
+    const line = document.createElementNS(svg.namespaceURI, "line");
+    line.setAttribute("stroke-width", "3");
+    line.setAttribute("stroke", color);
+    line.setAttribute("stroke-linecap", "round");
+    svg.prepend(line);
+    return line;
+}
+const linkFn = new Map();
+const linkFnInv = new Map();
+const symbolConstructor = new Map();
+const arrow = (ob1, ob2) => {
+    const prod = Symbol("prod");
+    symbolConstructor.set(prod, () => ({
+        cur: [undefined, undefined],
+        prev: [undefined, undefined],
+    }));
+    const pe1 = edge(ob1, prod);
+    linkFn.set(pe1, (o1, { cur, prev }) => {
+        if (prev[0] === undefined)
+            prev[0] = _(id)(id);
+        if (cur[0] !== undefined)
+            assign(prev[0])(cur[0]);
+        if (cur[0] === undefined)
+            cur[0] = _(id)(id);
+        if (approxEq(o1)(cur[0]))
+            return false;
+        assign(cur[0])(o1);
+    });
+    linkFnInv.set(pe1, ({ cur, prev }, o1) => {
+        if (cur[0] === undefined ||
+            (prev[0] !== undefined && approxEq(cur[0])(prev[0])))
+            return false;
+        if (approxEq(o1)(cur[0]))
+            return false;
+        assign(o1)(cur[0]);
+    });
+    const pe2 = edge(ob2, prod);
+    linkFn.set(pe2, (o2, { cur, prev }) => {
+        if (prev[1] === undefined)
+            prev[1] = _(id)(id);
+        if (cur[1] !== undefined)
+            assign(prev[1])(cur[1]);
+        if (cur[1] === undefined)
+            cur[1] = _(id)(id);
+        if (approxEq(o2)(cur[1]))
+            return false;
+        assign(cur[1])(o2);
+    });
+    linkFnInv.set(pe2, ({ cur, prev }, o2) => {
+        if (cur[1] === undefined ||
+            (prev[1] !== undefined && approxEq(cur[1])(prev[1])))
+            return false;
+        if (approxEq(o2)(cur[1]))
+            return false;
+        assign(o2)(cur[1]);
+    });
+    return [prod, pe1, pe2];
+};
+const N = Symbol("N");
+symbolConstructor.set(N, () => _(id)(id));
+const ar1 = arrow(N, N);
+const ar2 = arrow(N, N);
+const botLeftT = translation([-10, 10]);
+const e1 = edge(N, N, () => [ar1[1], inv(ar1[2])]);
+linkFn.set(e1, (t1, t2) => {
+    const newT = _(botLeftT)(t1);
+    if (approxEq(t2)(newT))
+        return false;
+    assign(t2)(newT);
+});
+linkFnInv.set(e1, (t2, t1) => {
+    const newT = _(t2)(tInv(botLeftT));
+    if (approxEq(t1)(newT))
+        return false;
+    assign(t1)(newT);
+});
+const botRightT = translation([10, 10]);
+const e2 = edge(N, N, () => [ar2[1], inv(ar2[2])]);
+linkFn.set(e2, (t1, t2) => {
+    const newT = _(botRightT)(t1);
+    if (approxEq(newT)(t2))
+        return false;
+    assign(t2)(newT);
+});
+linkFnInv.set(e2, (t2, t1) => {
+    const newT = _(t2)(tInv(botRightT));
+    if (approxEq(newT)(t1))
+        return false;
+    assign(t1)(newT);
+});
+const Nsvg = Symbol("Nsvg");
+symbolConstructor.set(Nsvg, () => makeCircleSvgEl(randomNiceCssOklab()));
+const nsvgE = edge(N, Nsvg);
+linkFn.set(nsvgE, (t, svg) => {
+    svg.setAttribute("cx", t[4]);
+    svg.setAttribute("cy", t[5]);
+    return false;
+});
+linkFnInv.set(nsvgE, (svg, t) => {
+    t[4] = Number(svg.getAttribute("cx"));
+    t[5] = Number(svg.getAttribute("cy"));
+});
+const ar1svg = Symbol("ar1svg");
+symbolConstructor.set(ar1svg, () => makeLineSvgEl("black"));
+const ar1svgE = edge(ar1[0], ar1svg);
+linkFn.set(ar1svgE, ({ cur }, svg) => {
+    if (cur[0] === undefined || cur[1] === undefined)
+        return false;
+    svg.setAttribute("x1", cur[0][4]);
+    svg.setAttribute("y1", cur[0][5]);
+    svg.setAttribute("x2", cur[1][4]);
+    svg.setAttribute("y2", cur[1][5]);
+    return false;
+});
+linkFnInv.set(ar1svgE, (svg, { cur, prev }) => {
+    assign(prev[0])(cur[0]);
+    assign(cur[0])([
+        cur[0][0],
+        cur[0][1],
+        cur[0][2],
+        cur[0][3],
+        Number(svg.getAttribute("x1")),
+        Number(svg.getAttribute("y1")),
+    ]);
+    assign(prev[1])(cur[1]);
+    assign(cur[1])([
+        cur[1][0],
+        cur[1][1],
+        cur[1][2],
+        cur[1][3],
+        Number(svg.getAttribute("x2")),
+        Number(svg.getAttribute("y2")),
+    ]);
+});
+const ar2svg = Symbol("ar2svg");
+symbolConstructor.set(ar2svg, () => makeLineSvgEl("black"));
+const ar2svgE = edge(ar2[0], ar2svg);
+linkFn.set(ar2svgE, ({ cur }, svg) => {
+    if (cur[0] === undefined || cur[1] === undefined)
+        return false;
+    svg.setAttribute("x1", cur[0][4]);
+    svg.setAttribute("y1", cur[0][5]);
+    svg.setAttribute("x2", cur[1][4]);
+    svg.setAttribute("y2", cur[1][5]);
+    return false;
+});
+linkFnInv.set(ar2svgE, (svg, { cur, prev }) => {
+    assign(prev[0])(cur[0]);
+    assign(cur[0])([
+        cur[0][0],
+        cur[0][1],
+        cur[0][2],
+        cur[0][3],
+        Number(svg.getAttribute("x1")),
+        Number(svg.getAttribute("y1")),
+    ]);
+    assign(prev[1])(cur[1]);
+    assign(cur[1])([
+        cur[1][0],
+        cur[1][1],
+        cur[1][2],
+        cur[1][3],
+        Number(svg.getAttribute("x2")),
+        Number(svg.getAttribute("y2")),
+    ]);
+});
+function* propagateForward(start, startData = symbolConstructor.get(start.symbol)()) {
+    const nodeData = new Map([[start, startData]]);
+    const queue = [start];
+    while (queue.length !== 0) {
+        const currentNode = queue.shift();
+        //outgoing edges only for now
+        for (const [edge, toContainer] of currentNode.to.entries()) {
+            const fn = linkFn.get(edge);
+            const fromData = nodeData.get(currentNode);
+            const to = toContainer.get();
+            const toData = nodeData.get(to) ??
+                nodeData.set(to, symbolConstructor.get(to.symbol)()).get(to);
+            const optionalPropagate = fn(fromData, toData);
+            if (optionalPropagate !== false)
+                queue.push(to);
+        }
+        yield currentNode;
+    }
+}
+function* propagate(start, startData = symbolConstructor.get(start.symbol)()) {
+    const nodeData = new Map([[start, startData]]);
+    const queue = [start];
+    while (queue.length !== 0) {
+        const currentNode = queue.shift();
+        for (const [edge, toContainer] of currentNode.to.entries()) {
+            const fn = linkFn.get(edge);
+            const from = currentNode;
+            const fromData = nodeData.get(from);
+            const to = toContainer.get();
+            const toData = nodeData.get(to) ??
+                nodeData.set(to, symbolConstructor.get(to.symbol)()).get(to);
+            const optionalPropagate = fn(fromData, toData);
+            if (optionalPropagate !== false)
+                queue.push(to);
+        }
+        for (const [edge, fromContainer] of currentNode.from.entries()) {
+            const fn = linkFnInv.get(edge);
+            const to = currentNode;
+            const toData = nodeData.get(to);
+            const from = fromContainer.get();
+            const fromData = nodeData.get(from) ??
+                nodeData.set(from, symbolConstructor.get(from.symbol)()).get(from);
+            const optionalPropagate = fn(toData, fromData);
+            if (optionalPropagate !== false)
+                queue.push(from);
+        }
+        yield currentNode;
+    }
+}
+// test
+const n = create(N);
+const propGen = propagate(n, translation(v(100)));
+function draw() {
+    requestAnimationFrame(draw);
+    propGen.next().value;
+}
+draw();
+// - should I add built-in change detection to propagate?
+//   - or should I research graph analysis methods to avoid backtracking?
+//     - the issue is from products
+//   why?
+//   - need the code to be heavily simplified so I can add more complex features
+//   alternatively I can just abstract away the isomorphisms but it will be messier.
---
+++ src/demo/2024_12/algae/index.ts
@@ -0,0 +1,296 @@
+// want each drawable to have its x and y filled in
+// before being drawn
+// for now we couuld give each drawable a renderInfo map
+// actually we can make draw be called with the result of
+// the things it depends on
+
+import { SetMap } from "../../../lib/structure/data.js";
+
+type Val = { val?: any };
+type Drawable = {
+  w: Val;
+  h: Val;
+  x: Val;
+  y: Val;
+};
+
+const onSet = new SetMap<Val, Function>();
+const addOnSet = (ob: Val, f: Function) => onSet.add(ob, f);
+const getOnSet = (ob: Val) => onSet.get(ob) ?? [];
+
+const set = (ob: Val) => (v: any) => {
+  if (ob.val !== undefined) return;
+  ob.val = v;
+  for (const f of getOnSet(ob)) f(v);
+};
+const eq = (ob1: Val, ob2: Val) => {
+  addOnSet(ob1, set(ob2));
+  addOnSet(ob2, set(ob1));
+};
+// when ob1 is set, set ob2
+const toEq = (ob2: Val, ob1: Val) => {
+  addOnSet(ob1, set(ob2));
+};
+const all = (...obs: Val[]) => {
+  const allOb = {};
+  const afterAll = () => {
+    if (obs.every(({ val }) => val !== undefined))
+      set(allOb)(obs.map(({ val }) => val!));
+  };
+  for (const ob of obs) addOnSet(ob, afterAll);
+  return allOb;
+};
+const map = (ob: Val) => (f: (v: any) => any) => {
+  const mappedOb = {};
+  addOnSet(ob, (v) => set(mappedOb)(f(v)));
+  return mappedOb;
+};
+const mapAll =
+  (...obs: Val[]) =>
+  (f: (...v: any[]) => any) => {
+    const mappedOb = {};
+    addOnSet(all(...obs), (v) => set(mappedOb)(f(...v)));
+    return mappedOb;
+  };
+const add = (...obs: Val[]) =>
+  mapAll(...obs)((...vs) => vs.reduce((s, v) => s + v));
+const mul = (...obs: Val[]) =>
+  mapAll(...obs)((...vs) => vs.reduce((s, v) => s * v));
+const min = (...obs: Val[]) => mapAll(...obs)(Math.min);
+const max = (...obs: Val[]) => mapAll(...obs)(Math.max);
+const sub = (ob1: Val, ob2: Val) => mapAll(ob1, ob2)((v1, v2) => v1 - v2);
+const div = (ob1: Val, ob2: Val) => mapAll(ob1, ob2)((v1, v2) => v1 / v2);
+
+// TODO: defaults
+// TODO?: ownership
+
+const drawable = (
+  draw: (x: number, y: number, w: number, h: number) => void
+) => {
+  const x: Val = {};
+  const y: Val = {};
+  const w: Val = {};
+  const h: Val = {};
+  mapAll(x, y, w, h)(draw);
+
+  let prevP;
+  let done = false;
+  const px = (v: number) => {
+    // if (done) throw "cannot set so many p"; // TODO: but what about get?
+    const pv: Val = {};
+
+    toEq(pv, add(x, mul({ val: v }, w)));
+    toEq(w, mul({ val: 1 / v }, sub(pv, x)));
+    toEq(x, sub(pv, mul({ val: v }, w)));
+
+    if (prevP !== undefined) {
+      const [v2, pv2] = prevP;
+      toEq(w, mul({ val: 1 / (v2 - v) }, sub(pv2, pv)));
+      done = true;
+    }
+    prevP = [v, pv];
+    return pv;
+  };
+  // const x2 = p(1);
+  // const c = p(0.5);
+
+  return { w, h, x, y, px };
+};
+const group = (...ds: Drawable[]): Drawable => {
+  const minY = min(...ds.map((d) => d.y));
+  const maxY = max(...ds.map((d) => add(d.y, d.h)));
+  const minX = min(...ds.map((d) => d.x));
+  const maxX = max(...ds.map((d) => add(d.x, d.w)));
+  const h = sub(maxY, minY);
+  const w = sub(maxX, minX);
+  const x = minX;
+  const y = minY;
+
+  return { x, y, w, h };
+};
+
+const stackPadV =
+  (pad: number) =>
+  (...ds: Drawable[]): Drawable => {
+    // TODO: make reverse w and x work
+    // if something outside the stack sets the stack's x:
+    // then check that this doesn't make anything in the stack fall outside of its width
+    // and change the defaults for things in the stack so they don't default to outside of it
+    for (let i = 0; i < ds.length - 1; i++) {
+      const cur = ds[i];
+      const next = ds[i + 1];
+      toEq(next.y, add(cur.y, cur.h, { val: pad }));
+      toEq(cur.y, add(sub(next.y, cur.h), { val: pad }));
+    }
+    return group(...ds);
+  };
+const stackV = stackPadV(30);
+const alignH =
+  (zeroToOneAlign: number) =>
+  (...ds: Drawable[]): Drawable => {
+    // only one x should be set, create a loop to set all others
+    for (let i = 0; i < ds.length; i++) {
+      const cur = ds.at(i)!;
+      const next = ds.at((i + 1) % ds.length)!;
+      const curC = add(
+        cur.x,
+        map(cur.w)((w) => w * zeroToOneAlign)
+      );
+      const nextHalfW = map(next.w)((w) => w * zeroToOneAlign);
+      toEq(next.x, sub(curC, nextHalfW));
+    }
+    return group(...ds);
+  };
+const centerH = alignH(0.5);
+const stackH = (...ds: Drawable[]): Drawable => {
+  const h = map(all(...ds.flatMap((d) => [d.y, d.h])))((yhs) => {
+    let minY = Infinity;
+    let maxY = -Infinity;
+    for (let i = 0; i < yhs.length; i += 2) {
+      const y = yhs[i];
+      const h = yhs[i + 1];
+
+      minY = Math.min(minY, y);
+      maxY = Math.max(maxY, y + h);
+    }
+    return maxY - minY;
+  });
+  const y = map(all(...ds.map((d) => d.y)))((vs) => Math.min(...vs));
+
+  const w = map(all(...ds.map((d) => d.w)))((ws) =>
+    ws.reduce((acc, w) => acc + w, 0)
+  );
+  const x = ds[0].x;
+  for (let i = 0; i < ds.length - 1; i++) {
+    toEq(
+      ds[i + 1].x,
+      map(all(ds[i].x, ds[i].w))(([x, w]) => w + x)
+    );
+    toEq(
+      ds[i].x,
+      map(all(ds[i + 1].x, ds[i].w))(([x, w]) => x - w)
+    );
+  }
+  return {
+    w,
+    h,
+    x,
+    y,
+  };
+};
+
+const c = document.getElementById("c");
+const ctx = c.getContext("2d");
+
+const d1 = drawable((...vs) => {
+  ctx.fillStyle = "orange";
+  ctx.fillRect(...vs);
+});
+const d2 = drawable((...vs) => {
+  ctx.fillStyle = "violet";
+  ctx.fillRect(...vs);
+});
+const d3 = drawable((...vs) => {
+  ctx.fillStyle = "CornflowerBlue";
+  ctx.fillRect(...vs);
+});
+const d4 = drawable((...vs) => {
+  ctx.fillStyle = "YellowGreen";
+  ctx.fillRect(...vs);
+});
+const v = stackV(d1, d2, d4);
+stackH(v, d3);
+centerH(d1, d2, d4);
+
+mapAll(
+  d2.x,
+  d2.y,
+  d3.x,
+  d3.y
+)((x1, y1, x2, y2) => {
+  ctx.strokeStyle = "white";
+  ctx.lineWidth = 5;
+  ctx.beginPath();
+  ctx.lineTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
+});
+
+const b = group(d1, d2, d3, v);
+mapAll(
+  b.x,
+  b.y,
+  b.w,
+  b.h
+)((x, y, w, h) => {
+  ctx.strokeStyle = "white";
+  ctx.lineWidth = 5;
+  ctx.beginPath();
+  ctx.lineTo(x, y);
+  ctx.lineTo(x + w, y);
+  ctx.lineTo(x + w, y + h);
+  ctx.lineTo(x, y + h);
+  ctx.closePath();
+  ctx.stroke();
+});
+
+set(d1.h)(100);
+set(d1.w)(100);
+set(d2.h)(100);
+set(d2.w)(200);
+set(d3.h)(100);
+set(d3.w)(100);
+set(d4.h)(20);
+set(d4.w)(20);
+
+set(d1.y)(100);
+// set(d1.x)(100);
+set(d2.x)(120);
+set(d3.y)(0);
+
+const d5 = drawable((...vs) => {
+  ctx.fillStyle = "red";
+  ctx.fillRect(...vs);
+});
+const d6 = drawable((...vs) => {
+  ctx.fillStyle = "blue";
+  ctx.fillRect(...vs);
+});
+const d7 = drawable((...vs) => {
+  ctx.fillStyle = "green";
+  ctx.fillRect(...vs);
+});
+const d8 = drawable((...vs) => {
+  ctx.fillStyle = "yellow";
+  ctx.fillRect(...vs);
+});
+
+const doLater: Function[] = [];
+const tree = (...ts) => {
+  const d = drawable((...vs) => {
+    ctx.fillStyle = "violet";
+    ctx.fillRect(...vs);
+  });
+  console.log("hi", d);
+  doLater.push(() => {
+    set(d.h)(10);
+    set(d.w)(10);
+  });
+  if (ts.length > 0) {
+    const children = stackH(...ts);
+    stackPadV(20)(d, children);
+    centerH(d, children);
+  }
+  return d;
+};
+
+const root = tree(tree());
+set(root.x)(100);
+set(root.y)(100);
+
+for (const f of doLater) f();
+
+const d = drawable(() => {});
+map(d.px(1))(console.log);
+set(d.px(0.5))(0);
+set(d.x)(-8);

5d94924e04b0dd36ea3e62c4bab3185288dd11cb
vezwork <key.draw@gmail.com>
2025-05-10 23:51:28 -0400

    Add link to alga_canvas in doc and add arithmetic example

---
+++ src/demo/2025_5/alga/future_tests/arithmetic.js
@@ -0,0 +1,16 @@
+// const uppy = upRel(
+//   (a, b) => a + b,
+//   (a, d, b) => a + d / 2
+// );
+// const a = Ob(10);
+// const b = Ob(20);
+// const ab = uppy(a, b);
+// const c = Ob(30);
+// const abc = uppy(ab, c);
+// console.log(a, b, ab, c, abc);
+// set(ab, 100);
+// console.log(a, b, ab, c, abc);
+// set(a, 20);
+// console.log(a, b, ab, c, abc);
+// set(abc, 0);
+// console.log(a, b, ab, c, abc);
